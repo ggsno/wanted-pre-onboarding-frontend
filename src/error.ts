ed2850d2ab5a7f02f54b6622944b3231128edf34
ggsno <ogs0518@gmail.com>
2023-02-05 12:37:06 +0900

    fix: 에러 핸들링 수정

--- src/error.ts
+++ src/error.ts
@@ -2,7 +2,8 @@ import { AxiosError } from "axios";
 import { toast } from "react-hot-toast";
 
 export function handleError(error: unknown) {
-  if (error instanceof AxiosError) {
+  console.error(error);
+  if (error instanceof AxiosError && error.response?.data.message) {
     toast.error(error.response?.data.message);
   } else if (error instanceof Error) {
     toast.error(error.message);
--- src/services/apis/auth.ts
+++ src/services/apis/auth.ts
@@ -1,14 +1,22 @@
 import axios from "axios";
+import { handleError } from "../../error";
 import { SignRequestProps, SignResponseProps } from "../model/authSchema";
+
+export function fetchSignIn(props: SignRequestProps) {
+  return getInstance().post<SignResponseProps>("signin", props);
+}
+
+export function fetchSignup(props: SignRequestProps) {
+  return getInstance().post<SignResponseProps>("signup", props);
+}
 
 const instance = axios.create({
   baseURL: `${process.env.REACT_APP_BACKEND_URL}/auth/`,
 });
 
-export function fetchSignIn(props: SignRequestProps) {
-  return instance.post<SignResponseProps>("signin", props);
-}
+const getInstance = () => {
+  if (!process.env.REACT_APP_BACKEND_URL)
+    handleError(new Error("no environment variable : REACT_APP_BACKEND_URL"));
 
-export function fetchSignup(props: SignRequestProps) {
-  return instance.post<SignResponseProps>("signup", props);
-}
+  return instance;
+};
